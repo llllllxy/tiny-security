3758d5e932883a8855acd3fc2ded3e89177c2aa6
17862719591 <184974699@qq.com>
2023-06-02 14:49:40 +0800

    修改自动配置的类的写法，这样更加的优雅

--- src/main/java/org/bluewind/authclient/AuthAutoConfiguration.java
+++ src/main/java/org/bluewind/authclient/AuthAutoConfiguration.java
@@ -9,50 +9,31 @@ import org.bluewind.authclient.provider.RedisAuthProvider;
 import org.bluewind.authclient.provider.SingleAuthProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.Collection;
-
+/**
+ * <p>
+ *     自动配置类
+ * </p>
+ * @author liuxingyu01
+ * @since  2022-12-13 11:45
+ **/
 @Configuration
 @EnableConfigurationProperties(AuthProperties.class)
 @ConditionalOnProperty(name = "authclient.enable", havingValue = "true")
-public class AuthAutoConfiguration implements ApplicationContextAware {
+public class AuthAutoConfiguration {
     final static Logger logger = LoggerFactory.getLogger(AuthAutoConfiguration.class);
-
-    private ApplicationContext applicationContext;
 
     @Autowired
     private AuthProperties authProperties;
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
-
-
-    /**
-     * 根据Class<T>获取Bean
-     */
-    private <T> T getBean(Class<T> clazz) {
-        T bean = null;
-        Collection<T> beans = applicationContext.getBeansOfType(clazz).values();
-        while (beans.iterator().hasNext()) {
-            bean = beans.iterator().next();
-            if (bean != null) break;
-        }
-        return bean;
-    }
 
 
     /**
@@ -61,11 +42,11 @@ public class AuthAutoConfiguration implements ApplicationContextAware {
     @ConditionalOnMissingBean(AuthProvider.class)
     @ConditionalOnProperty(name = "authclient.store-type", havingValue = "redis")
     @Bean
-    public AuthProvider redisAuthProvider() {
-        StringRedisTemplate stringRedisTemplate = getBean(StringRedisTemplate.class);
+    public AuthProvider redisAuthProvider(StringRedisTemplate stringRedisTemplate) {
         if (stringRedisTemplate == null) {
             logger.error("AuthAutoConfiguration: Bean StringRedisTemplate is null");
         }
+        logger.info("RedisAuthProvider is running");
         return new RedisAuthProvider(stringRedisTemplate, authProperties);
     }
 
@@ -75,11 +56,11 @@ public class AuthAutoConfiguration implements ApplicationContextAware {
     @ConditionalOnMissingBean(AuthProvider.class)
     @ConditionalOnProperty(name = "authclient.store-type", havingValue = "jdbc")
     @Bean
-    public AuthProvider jdbcAuthProvider() {
-        JdbcTemplate jdbcTemplate = getBean(JdbcTemplate.class);
+    public AuthProvider jdbcAuthProvider(JdbcTemplate jdbcTemplate) {
         if (jdbcTemplate == null) {
             logger.error("AuthAutoConfiguration: Bean JdbcTemplate is null");
         }
+        logger.info("JdbcAuthProvider is running");
         return new JdbcAuthProvider(jdbcTemplate, authProperties);
     }
 
@@ -90,17 +71,16 @@ public class AuthAutoConfiguration implements ApplicationContextAware {
     @ConditionalOnProperty(name = "authclient.store-type", havingValue = "single")
     @Bean
     public AuthProvider singleAuthProvider() {
+        logger.info("SingleAuthProvider is running");
         return new SingleAuthProvider(authProperties);
     }
 
 
     /**
-     * 添加会话拦截器
+     * 添加会话拦截器( 注入AuthStore（可能是redis的，也可能是jdbc的，根据配置来的）)
      */
     @Bean
-    public AuthenticeInterceptor authenticeInterceptor() {
-        // 获取AuthStore（可能是redis的，也可能是jdbc的，根据配置来的）
-        AuthProvider authProvider = getBean(AuthProvider.class);
+    public AuthenticeInterceptor authenticeInterceptor(AuthProvider authProvider) {
         if (authProvider != null) {
             return new AuthenticeInterceptor(authProvider);
         } else {
@@ -112,12 +92,11 @@ public class AuthAutoConfiguration implements ApplicationContextAware {
 
     /**
      * 添加权限拦截器（当存在bean PermissionInfoInterface时，这个配置才生效）
+     * 注入PermissionInfoInterface
      */
     @Bean
     @ConditionalOnBean(PermissionInfoInterface.class)
-    public PermissionInterceptor permissionInterceptor() {
-        // 获取PermissionInfoInterface
-        PermissionInfoInterface permissionInfoInterface = getBean(PermissionInfoInterface.class);
+    public PermissionInterceptor permissionInterceptor(PermissionInfoInterface permissionInfoInterface) {
         if (permissionInfoInterface != null) {
             return new PermissionInterceptor(permissionInfoInterface);
         } else {
