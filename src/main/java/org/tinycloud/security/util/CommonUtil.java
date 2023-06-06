fa28d0abe6f5e33bba9299c2c1293a1d7b7c3149
17862719591 <184974699@qq.com>
2023-06-06 14:05:55 +0800

    feat: getRandomString方法增加下划线的随机

--- src/main/java/org/tinycloud/security/util/CommonUtil.java
+++ src/main/java/org/tinycloud/security/util/CommonUtil.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 一些公共的，不好分类的工具方法放在这里
+ *
  * @author liuxingyu01
  * @version 2023-01-06-9:33
  **/
@@ -40,29 +41,28 @@ public class CommonUtil {
     }
 
     /**
-     * 生成指定长度的随机字符串
+     * 生成指定长度的随机字符串（包括大小写字母，数字和下划线）
      *
      * @param length 字符串的长度
      * @return 一个随机字符串
      */
     public static String getRandomString(int length) {
-        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            int number = ThreadLocalRandom.current().nextInt(62);
+            int number = ThreadLocalRandom.current().nextInt(63);
             sb.append(str.charAt(number));
         }
         return sb.toString();
     }
 
     /**
-     *
      * 字符串转时间 然后比较大小
      *
      * @param tempTime 要比较的时间字符串，格式为yyyyMMddHHmmss，
      * @return true：输入时间大于当前时间 ，false：输入时间小于当前时间
      */
-    public static boolean timeCompare (String tempTime) {
+    public static boolean timeCompare(String tempTime) {
         DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern(TIME_FORMAT);
         LocalDateTime localDateTime = LocalDateTime.parse(tempTime, dtf2);
         return localDateTime.isAfter(LocalDateTime.now());
