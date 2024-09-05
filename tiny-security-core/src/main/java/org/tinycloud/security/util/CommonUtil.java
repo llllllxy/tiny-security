f5954a4dca0aad770735de401224e68aeb9fe8ac
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-09-05 10:46:21 +0800

    feat: 删除无用的方法

--- tiny-security-core/src/main/java/org/tinycloud/security/util/CommonUtil.java
+++ tiny-security-core/src/main/java/org/tinycloud/security/util/CommonUtil.java
@@ -12,9 +12,7 @@ import java.util.concurrent.ThreadLocalRandom;
  * @version 2023-01-06-9:33
  **/
 public class CommonUtil {
-
     private final static String TIME_FORMAT = "yyyyMMddHHmmss";
-
 
     /**
      * 获取当前时间 比如 DateTool.getCurrentTime(); 返回值为 20120515234420
@@ -25,19 +23,6 @@ public class CommonUtil {
         LocalDateTime dateTime = LocalDateTime.now();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
         return dateTimeFormatter.format(dateTime);
-    }
-
-    /**
-     * 在当前时间上增加 若干秒
-     *
-     * @param secondsCount 要增加的秒数
-     * @return 固定格式 yyyyMMddHHmmss
-     */
-    public static String currentTimePlusSeconds(int secondsCount) {
-        LocalDateTime time = LocalDateTime.now();
-        LocalDateTime newTime = time.plusSeconds(secondsCount); // 增加几秒
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
-        return dateTimeFormatter.format(newTime);
     }
 
     /**
@@ -52,7 +37,6 @@ public class CommonUtil {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
         return dateTimeFormatter.format(newTime);
     }
-
 
     /**
      * 生成指定长度的随机字符串（包括大小写字母，数字和下划线）
@@ -69,17 +53,4 @@ public class CommonUtil {
         }
         return sb.toString();
     }
-
-    /**
-     * 字符串转时间 然后比较大小
-     *
-     * @param tempTime 要比较的时间字符串，格式为yyyyMMddHHmmss，
-     * @return true：输入时间大于当前时间 ，false：输入时间小于当前时间
-     */
-    public static boolean timeCompare(String tempTime) {
-        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern(TIME_FORMAT);
-        LocalDateTime localDateTime = LocalDateTime.parse(tempTime, dtf2);
-        return localDateTime.isAfter(LocalDateTime.now());
-    }
-
 }
