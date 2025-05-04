132199be339b5f6fe784e8c5aa44a9b8a75496aa
llllllxy <34022230+llllllxy@users.noreply.github.com>
2025-05-04 18:24:10 +0800

    feat: 增加版本号自动获取工具

---
+++ tiny-security-core/src/main/java/org/tinycloud/security/util/VersionUtil.java
@@ -0,0 +1,32 @@
+package org.tinycloud.security.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * <p>
+ * 获取版本号
+ * </p>
+ *
+ * @author liuxingyu01
+ * @since 2024-04-16 11:11
+ */
+public class VersionUtil {
+    private VersionUtil() {
+    }
+
+    public static String getVersion() {
+        String appVersion = "";
+        Properties properties = new Properties();
+        try (InputStream stream = VersionUtil.class.getClassLoader().getResourceAsStream("core.properties")) {
+            properties.load(stream);
+            if (!properties.isEmpty()) {
+                appVersion = properties.getProperty("core.version");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("getVersion failed: ", e);
+        }
+        return appVersion;
+    }
+}
