90d29503444a21c02764ec3eb63a5ddc763e7e52
llllllxy <184974699@qq.com>
2025-11-24 17:54:15 +0800

    feat：优化权限校验代码和注释

--- tiny-security-core/src/main/java/org/tinycloud/security/util/CommonUtil.java
+++ tiny-security-core/src/main/java/org/tinycloud/security/util/CommonUtil.java
@@ -138,15 +138,8 @@ public class CommonUtil {
             return false;
         }
         for (String configPath : configPaths) {
-            if (configPath.equals(requestPath)) {
+            if (StringUtils.hasLength(configPath) && MATCHER.match(configPath, requestPath)) {
                 return true;
-            } else {
-                boolean isPattern = MATCHER.isPattern(configPath);
-                if (isPattern) {
-                    if (MATCHER.match(configPath, requestPath)) {
-                        return true;
-                    }
-                }
             }
         }
         return false;
