8eb95a5b35c23c0f76b7f97fe130f147f37e2365
llllllxy <llllllxy@users.noreply.github.com>
2026-06-08 21:16:35 +0800

    feat: 1.3.0版本发布

--- tiny-security-core/src/main/java/org/tinycloud/security/exception/TinySecurityException.java
+++ tiny-security-core/src/main/java/org/tinycloud/security/exception/TinySecurityException.java
@@ -3,10 +3,8 @@ package org.tinycloud.security.exception;
 
 import org.tinycloud.security.consts.AuthConsts;
 
-import java.io.Serial;
 
 public class TinySecurityException extends RuntimeException {
-    @Serial
     private static final long serialVersionUID = 2413958299445359500L;
 
     private int code;
